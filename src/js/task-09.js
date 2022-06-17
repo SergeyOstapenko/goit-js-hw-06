0c92ea4fb481057a205265c0d0f97862585d50f9
SergeyOstapenko <sergiy.v.ostapenko@gmail.com>
2022-06-17 20:23:25 +0300

    task 1-9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+const btn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+const bcgBody = document.querySelector('body');
+
+const btnClick = addEventListener('click', onBtnClickChangeColor);
+
+function onBtnClickChangeColor(event) {
+  const bgc = bcgBody.style.backgroundColor = getRandomHexColor();
+
+  console.log(bcgBody);
+   return colorName.textContent = bgc;
 }
